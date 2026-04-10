8795061525b70cada5d131ab168ab90df178348e
Faez Mahmud <faezmahmud376@gmail.com>
2026-04-11 03:19:06 +0600

    .

--- app/__init__.py
+++ app/__init__.py
@@ -188,10 +188,10 @@ def create_app():
 
     @app.before_request
     def maintenance_mode():
-    if (
+       if (
         os.getenv("MAINTENANCE_MODE", "").lower() in {"1", "true", "yes"}
         and not (current_user.is_authenticated and current_user.email == app.config["ADMIN_EMAIL"])
-    ):
+     ):
         return (
             render_template(
                 "error.html",
